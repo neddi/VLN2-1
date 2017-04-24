c642585c8220dbbfaadb07725ecedd2cc5482e6e
Gunnar Gunnarsson <gunnargun16@ru.is>
2017-04-24 10:12:31 +0000

    Grunn project sett inn

---
+++ ProgramWeb/ProgramWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgramWeb.Startup))]
+namespace ProgramWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
