a15f113f9679b350db1b753cd233b68b493acd0d
styrmird <styrmir16@ru.is>
2017-05-05 09:25:43 +0000

    missed merg in first resolution

--- ProgramWeb/ProgramWeb/Services/UserService.cs
+++ ProgramWeb/ProgramWeb/Services/UserService.cs
@@ -1,8 +1,5 @@
 ﻿using ProgramWeb.Models;
-<<<<<<< HEAD
-=======
 using ProgramWeb.Models.ViewModel;
->>>>>>> ec97120f950b5b40c58a5a7d74da57ccb7792e7a
 using System;
 using System.Collections.Generic;
 using System.Linq;
