97548c5b19a2462aae143b82196dcdf361710c02
funi16 <funi16@ru.is>
2017-05-08 12:00:08 +0000

    Komid med partial view

--- ProgramWeb/ProgramWeb/Controllers/NavigationController.cs
+++ ProgramWeb/ProgramWeb/Controllers/NavigationController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProgramWeb.Models.ViewModel;
 
 namespace ProgramWeb.Controllers
 {
     public class NavigationController : Controller
     {
         // GET: Navigation
-        public ActionResult Index()
+        public ActionResult Menu()
         {
-            return View();
+
+            return PartialView("_SideBarNavigation");
         }
     }
 }
+
+
