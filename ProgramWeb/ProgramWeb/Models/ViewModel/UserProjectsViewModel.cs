6cc9661b4de395dc6aa79a78afe94959f2f8ad4a
Gunnar Gunnarsson <gunnargun16@ru.is>
2017-05-08 10:45:37 +0000

    Unnið í UserProjects lista

---
+++ ProgramWeb/ProgramWeb/Models/ViewModel/UserProjectsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProgramWeb.Models.ViewModel
+{
+	public class UserProjectsViewModel
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string FullName { get; set; }
+		public List<ProjectViewModel> ProjectList { get; set; }
+	}
+}
