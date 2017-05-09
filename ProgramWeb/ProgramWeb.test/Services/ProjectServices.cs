ad77777bc0b2aba4b8e2f595345bd322bba21726
Gunnar Gunnarsson <gunnargun16@ru.is>
2017-05-09 11:13:29 +0000

    Byrjað að setja inn einingaprófanir

---
+++ ProgramWeb/ProgramWeb.test/Services/ProjectServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgramWeb.test.Services
+{
+	[TestClass]
+	public class ProjectServices
+	{
+		[TestMethod]
+		public void TestGetProject()
+		{
+			// Assign: Breytur sem notaðar eru við testið
+
+			// Act: Kallar í fallið sem á að testa
+
+			// Assert: Prófar útkomuna
+		}
+	}
+}
