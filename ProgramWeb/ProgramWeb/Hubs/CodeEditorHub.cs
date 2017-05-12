05a03382286a551a835f30e3c82f540a3bdf0652
Sigga <siggaosp@gmail.com>
2017-05-12 00:00:58 +0000

    Proof of Concept for SignalR

---
+++ ProgramWeb/ProgramWeb/Hubs/CodeEditorHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ProgramWeb.Hubs
+{
+    public class CodeEditorHub : Hub
+    {
+        public void UpdateContent(int fileID, string content)
+        {
+            Clients.All.UpdateContent(fileID, content);
+        }
+    }
+}
